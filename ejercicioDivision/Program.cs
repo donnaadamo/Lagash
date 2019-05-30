625354088bb1473ff3cc0c1d1e640871175bdee6
Lagash <Lagash@DESKTOP-ABROF0L.LagashSystems.local>
2019-05-30 20:56:14 -0300

    Commit test

---
+++ ejercicioDivision/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ejercicioDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cociente = 0;
+            int resto = 0;
+            int x = 20;
+            int y = 4;
+            
+            if (x != 0 && y != 0){
+                Console.WriteLine("Cociente: " + Dividir(x, y) + " resto: " + resto);
+            }else{
+                Console.WriteLine("Error: Uno o ambos parametros son 0");
+            }
+
+            int Dividir(int dividendo, int divisor){
+                resto = dividendo - divisor;
+                cociente ++;
+                if (resto<divisor){
+                    return cociente;
+                }else{
+                    return Dividir(resto, divisor);
+                }            
+            }
+        }
+    }
+}
