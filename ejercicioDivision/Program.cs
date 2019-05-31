96946b2c2e4128624e27d0d3288df179c604aed4
Lagash <Lagash@DESKTOP-ABROF0L.LagashSystems.local>
2019-05-31 14:47:21 -0300

    actualizacion division

--- ejercicioDivision/Program.cs
+++ ejercicioDivision/Program.cs
@@ -8,24 +8,31 @@ namespace ejercicioDivision
         {
             int cociente = 0;
             int resto = 0;
-            int x = 20;
-            int y = 4;
-            
-            if (x != 0 && y != 0){
-                Console.WriteLine("Cociente: " + Dividir(x, y) + " resto: " + resto);
-            }else{
-                Console.WriteLine("Error: Uno o ambos parametros son 0");
-            }
 
             int Dividir(int dividendo, int divisor){
+                if(dividendo != 0 && divisor != 0){
                 resto = dividendo - divisor;
                 cociente ++;
                 if (resto<divisor){
-                    return cociente;
+                        int result = cociente;
+                        cociente = 0;
+                        return result;
                 }else{
                     return Dividir(resto, divisor);
                 }            
+                }else{
+                    resto = 0;
+                    return 0;
+                }
             }
+
+                Console.WriteLine("Cociente: " + Dividir(9, 9) + " resto: " + resto);
+                Console.WriteLine("Cociente: " + Dividir(9, 6) + " resto: " + resto);
+                Console.WriteLine("Cociente: " + Dividir(9, 3) + " resto: " + resto);
+                Console.WriteLine("Cociente: " + Dividir(9, 2) + " resto: " + resto);
+                Console.WriteLine("Cociente: " + Dividir(0, 1) + " resto: " + resto);
+                Console.WriteLine("Cociente: " + Dividir(9, 0) + " resto: " + resto);
+            
         }
     }
 }
